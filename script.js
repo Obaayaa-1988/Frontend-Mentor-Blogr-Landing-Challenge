43ebced966c7458cf7029c84e284ea16fd90b005
Obaayaa-1988 <ashbella333@gmail.com>
2023-02-02 14:23:01 +0000

    added script to showcase dropdown

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // alert("hello world")
+/**adding script to dropdown menu and svg icon */
+
+const drop = document.getElementById("main-drop")
+const content = document.getElementById("content")
+
+drop.addEventListener('click', () => {
+    content.classList.toggle('show')
+
+})
+
+
